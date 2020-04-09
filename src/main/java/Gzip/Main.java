e17467f4bcce9e9a09a6aa6a77f738c24d5b533d
Alex <Alex@DESKTOP-8KOEPFS>
2020-04-09 17:46:09 +0200

    Création de la classe Main permettant de tester la classe Gzip selon 4 fichiers DAMIR.

---
+++ src/main/java/Gzip/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Gzip;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Alex
+ */
+public class Main {
+    //10001 premieres lignes
+    private static final String DAMIR_201812_SMALL = "https://mycore.core-cloud.net/index.php/s/j9DEDyt2w6FTx6P/download";
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        Gzip downloader=new Gzip();
+        downloader.readGzipURL(DAMIR_201812_SMALL);
+    }
+    
+}
