8e31b4ab7ad8523e6da20b02c89b543ecc46b60a
ulyss <ulyss@LAPTOP-OD9FT2I8>
2020-05-08 00:00:46 +0200

    JS

--- src/main/webapp/js/requetes.js
+++ src/main/webapp/js/requetes.js
@@ -1,59 +1,122 @@
-// cf. https://developers.google.com/chart/interactive/docs/gallery/geochart
-google.charts.load('current', {'packages': ['geochart']});
+//Doc Google Charts https://developers.google.com/chart/interactive/docs/gallery/geochart
 
+//Importer chaque type de Google Chart
+google.charts.load('current', {'packages': ['geochart']});
+google.charts.load('current', {'packages': ['corechart']});
+google.charts.load('current', {'packages': ['barchart']});
+
+//Savoir quelle requête doit être effectuée
 let requete = document.getElementById("requete").value;
 
-function drawGeoChart(result) {
+//Fonction pour créer et ajouter la Google Chart
+function drawChart(result) {
     if (requete === "requete1") {
-        // On met le résultat au format attendu par google
+        //On met le résultat au format attendu par google
         var data = [["Région","Montant"]];
-        result.forEach( ligne => data.push([ligne.region, ligne.montant)]));
-        titleData = "Montant des remboursements par Région";
+        //Ajouter au tableau de données les résultats du DTO
+        result.forEach( ligne => data.push([ligne.region, ligne.montant]));
+        //Convertir le tableau de données en table de données
+        var dataTable = google.visualization.arrayToDataTable(data);
+        //Créer un affichage dans le div d'id "affichage"
+        var geochart = new google.visualization.GeoChart(document.getElementById("affichage"));
+        //Définir le titre en fonction de la requête effectuée
+        var options = {title: "Montant des remboursements par Région"};
+        //Ajouter la geochart avec son titre
+        geochart.draw(dataTable, options);
     }
     if (requete === "requete2") {
-        // On met le résultat au format attendu par google
+        //On met le résultat au format attendu par google
         var data = [["Région","Tranche d'âge"]];
-        result.forEach( ligne => data.push([ligne.region, ligne.trancheAge)]));
-        titleData = "Tranche d’âge la plus remboursée par région";
+        //Ajouter au tableau de données les résultats du DTO
+        result.forEach( ligne => data.push([ligne.region, ligne.trancheAge]));
+        //Convertir le tableau de données en table de données
+        var dataTable = google.visualization.arrayToDataTable(data);
+        //Créer un affichage dans le div d'id "affichage"
+        var geochart = new google.visualization.GeoChart(document.getElementById("affichage"));
+        //Définir le titre en fonction de la requête effectuée
+        var options = {title: "Tranche d’âge la plus remboursée par région"};
+        //Ajouter la geochart avec son titre
+        geochart.draw(dataTable, options);
     }
     if (requete === "requete3") {
-        // On met le résultat au format attendu par google
+        //On met le résultat au format attendu par google
         var data = [["Nature de la prestation","Tranche d'âge"]];
-        result.forEach( ligne => data.push([ligne.nature, ligne.trancheAge)]));
-        titleData = "Nature de la prestation la plus remboursée par tranche d'âge";
+        //Ajouter au tableau de données les résultats du DTO
+        result.forEach( ligne => data.push([ligne.nature, ligne.trancheAge]));
+        //Convertir le tableau de données en table de données
+        var dataTable = google.visualization.arrayToDataTable(data);
+        //Créer un affichage dans le div d'id "affichage"
+        var piechart = new google.visualization.PieChart(document.getElementById("affichage"));
+        //Définir le titre en fonction de la requête effectuée
+        var options = {title: "Tranche d’âge la plus remboursée par région"};
+        //Ajouter le piechart avec son titre
+        piechart.draw(dataTable, options);
     }
     if (requete === "requete4") {
-        // On met le résultat au format attendu par google
+        //On met le résultat au format attendu par google
         var data = [["Montant", "Secteur", "Tanche d'âge"]];
-        result.forEach( ligne => data.push([ligne.montant, ligne.secteur, ligne.trancheAge)]));
-        titleData = "Montant des dépenses par secteur privé/public par tranche d'âge)";
+        //Ajouter au tableau de données les résultats du DTO
+        result.forEach( ligne => data.push([ligne.montant, ligne.secteur, ligne.trancheAge]));
+        //Obligatoire pour créer un barchart
+        var view = new google.visualization.DataView(data);
+        
+        //Définir les colonnes
+        view.setColumns([0, 1,
+            { calc: "stringify",
+              sourceColumn: 1,
+              type: "string",
+              role: "annotation" },
+            2]);
+        
+        var options = {
+            title: "Montant des dépenses par secteur privé/public par tranche d'âge)",
+            width: 600,
+            height: 400,
+            bar: {groupWidth: "95%"},
+            legend: { position: "none" },
+        };
+        //Créer un affichage dans le div d'id "affichage"
+        var barchart = new google.visualization.BarChart(document.getElementById("affichage"));
+        //Ajouter le piechart avec son titre
+        barchart.draw(view, options);
     }
     if (requete === "requete5") {
-        enTeteData = ["Dépense", "Remboursement", "Spécialité du médecin exécutant"];
-        pushData = ["ligne.depense", "ligne.remboursement", "ligne.specialite"];
-        titleData = "Montant moyen de la dépense et du remboursement par spécialité du médecin exécutant";
         // On met le résultat au format attendu par google
         var data = [["Dépense", "Remboursement", "Spécialité du médecin exécutant"]];
-        result.forEach( ligne => data.push([ligne.depense, ligne.remboursement, ligne.specialite)]));
-        titleData = "Montant des dépenses par secteur privé/public par tranche d'âge)";
+        //Ajouter au tableau de données les résultats du DTO
+        result.forEach( ligne => data.push([ligne.depense, ligne.remboursement, ligne.specialite]));
+        //Obligatoire pour créer un barchart
+        var view = new google.visualization.DataView(data);
+        //Définir les colonnes
+        view.setColumns([0, 1,
+            { calc: "stringify",
+              sourceColumn: 1,
+              type: "string",
+              role: "annotation" },
+            2]);
+        
+        var options = {
+            title: "Montant des dépenses par secteur privé/public par tranche d'âge)",
+            width: 600,
+            height: 400,
+            bar: {groupWidth: "95%"},
+            legend: { position: "none" },
+        };
+        //Créer un affichage dans le div d'id "affichage"
+        var barchart = new google.visualization.BarChart(document.getElementById("affichage"));
+        //Ajouter le piechart avec son titre
+        barchart.draw(view, options);
     }
-    var dataTable = google.visualization.arrayToDataTable(data);
-
-    var geochart = new google.visualization.GeoChart(document.getElementById("affichage"));
-    var options = {title: titleData};
-    chart.draw(dataTable, options);
-}
 
 function doAjax() {
     //On change l'url en fonction de la requete choisie
     let url = "service/" + requete;
-    
     $.ajax({
         url: url,
         // Les données saisies dans le formlaire
         data : $('#formulaireRequetes').serialize(),
         dataType: "json",
-        success: drawGeoChart, // En cas de succès, on crée la GeoChart
+        success: drawChart, // En cas de succès, on crée la GeoChart
         error: showError //En cas d'erreur, on affiche le message d'erreur
     });
 }
@@ -61,4 +124,5 @@ function doAjax() {
 // Fonction qui traite les erreurs de la requête
 function showError(xhr, status, message) {
     alert("Erreur: " + status + " : " + message);
+}
 }
