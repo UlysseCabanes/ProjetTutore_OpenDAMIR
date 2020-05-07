f94b882107ae6a7fe0c7efcdc316490693acbc18
ulyss <ulyss@LAPTOP-OD9FT2I8>
2020-05-07 18:54:43 +0200

    JS

--- src/main/webapp/js/requetes.js
+++ src/main/webapp/js/requetes.js
@@ -2,44 +2,44 @@
 google.charts.load('current', {'packages': ['geochart']});
 
 let requete = document.getElementById("requete").value;
-let enTeteData = new Array();
-let pushData = new Array();
-let titleData;
-
-if (requete === "requete1") {
-    enTeteData = ["Region", "Montant"];
-    pushData = ["ligne.region", "ligne.montant"];
-    titleData = "Montant des remboursements par Région";
-}
-if (requete === "requete2") {
-    enTeteData = ["Region", "Tranche d'âge"];
-    pushData = ["ligne.region", "ligne.trancheAge"];
-    titleData = "Tranche d’âge la plus remboursée par région";
-}
-if (requete === "requete3") {
-    enTeteData = ["Nature de la prestation", "Tranche d'âge"];
-    pushData = ["ligne.nature", "ligne.trancheAge"];
-    titleData = "Nature de la prestation la plus remboursée par tranche d'âge";
-}
-if (requete === "requete4") {
-    enTeteData = ["Montant", "Secteur", "Tanche d'âge"];
-    pushData = ["ligne.montant", "ligne.secteur", "ligne.trancheAge"];
-    titleData = "Montant des dépenses par secteur privé/public par tranche d'âge)";
-}
-if (requete === "requete5") {
-    enTeteData = ["Dépense", "Remboursement", "Spécialité du médecin exécutant"];
-    pushData = ["ligne.depense", "ligne.remboursement", "ligne.specialite"];
-    titleData = "Montant moyen de la dépense et du remboursement par spécialité du médecin exécutant";
-}
-
 
 function drawGeoChart(result) {
-    // On met le résultat au format attendu par google
-    var data = [[enTeteData]];
-    result.forEach( ligne => data.push([pushData.forEach(p => ligne.pushData[p])]));
+    if (requete === "requete1") {
+        // On met le résultat au format attendu par google
+        var data = [["Région","Montant"]];
+        result.forEach( ligne => data.push([ligne.region, ligne.montant)]));
+        titleData = "Montant des remboursements par Région";
+    }
+    if (requete === "requete2") {
+        // On met le résultat au format attendu par google
+        var data = [["Région","Tranche d'âge"]];
+        result.forEach( ligne => data.push([ligne.region, ligne.trancheAge)]));
+        titleData = "Tranche d’âge la plus remboursée par région";
+    }
+    if (requete === "requete3") {
+        // On met le résultat au format attendu par google
+        var data = [["Nature de la prestation","Tranche d'âge"]];
+        result.forEach( ligne => data.push([ligne.nature, ligne.trancheAge)]));
+        titleData = "Nature de la prestation la plus remboursée par tranche d'âge";
+    }
+    if (requete === "requete4") {
+        // On met le résultat au format attendu par google
+        var data = [["Montant", "Secteur", "Tanche d'âge"]];
+        result.forEach( ligne => data.push([ligne.montant, ligne.secteur, ligne.trancheAge)]));
+        titleData = "Montant des dépenses par secteur privé/public par tranche d'âge)";
+    }
+    if (requete === "requete5") {
+        enTeteData = ["Dépense", "Remboursement", "Spécialité du médecin exécutant"];
+        pushData = ["ligne.depense", "ligne.remboursement", "ligne.specialite"];
+        titleData = "Montant moyen de la dépense et du remboursement par spécialité du médecin exécutant";
+        // On met le résultat au format attendu par google
+        var data = [["Dépense", "Remboursement", "Spécialité du médecin exécutant"]];
+        result.forEach( ligne => data.push([ligne.depense, ligne.remboursement, ligne.specialite)]));
+        titleData = "Montant des dépenses par secteur privé/public par tranche d'âge)";
+    }
     var dataTable = google.visualization.arrayToDataTable(data);
 
-    var geochart = new google.visualization.GeoChart(document.getElementById('affichage'));
+    var geochart = new google.visualization.GeoChart(document.getElementById("affichage"));
     var options = {title: titleData};
     chart.draw(dataTable, options);
 }
@@ -47,14 +47,11 @@ function drawGeoChart(result) {
 function doAjax() {
     //On change l'url en fonction de la requete choisie
     let url = "service/" + requete;
-    let moisDebut = document.getElementById("moisDebut").value;
-    let anneeDebut = document.getElementById("anneeDebut").value;
-    let moisFin = document.getElementById("moisFin").value;
-    let anneeFin = document.getElementById("anneeFin").value;
+    
     $.ajax({
         url: url,
         // Les données saisies dans le formlaire
-        data : $('#formulaireCategorie').serialize(),
+        data : $('#formulaireRequetes').serialize(),
         dataType: "json",
         success: drawGeoChart, // En cas de succès, on crée la GeoChart
         error: showError //En cas d'erreur, on affiche le message d'erreur
